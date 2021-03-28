c4ffbd1e5d0b41dd64264d46cc122322fa17baad
zrp-unix <zaripoff.ashat@yandex.ru>
2021-03-28 13:55:04 +0300

    some methods of the point class have been added

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double[] x = {1, 2, 3};
-        double[] y = {4, 5, 6};
-        Point p = new Point(3, x);
-        p.setX(y);
-        System.out.print(p.getX(1));
+        //Main
     }
 }
 
