ab5202656fdd09b49c158ce20b5b2d31acfaf532
zrp-unix <zaripoff.ashat@yandex.ru>
2021-03-28 18:57:22 +0300

    i dunno does it work or not

--- src/Point.java
+++ src/Point.java
@@ -2,7 +2,7 @@ class  Point {
     private int dim;
     private double[] x = new double[3];
 
-    public Point(int dim) {
+    public Point(int dim, int[] ints) {
         this.dim = dim;
         for(int i = 0; i < dim; i++) {
             this.x[i] = 1;
@@ -15,11 +15,11 @@ class  Point {
     }
 
     public double[] getX() {
-        return this.x;
+        return x;
     }
 
     public double getX(int i) {
-        return this.x[i];
+        return x[i];
     }
 
     public void setX(double[] x) {
@@ -37,58 +37,123 @@ class  Point {
         }
         return Math.sqrt(res);
     }
+
     static Point add(Point a, Point b) {
-        //return a + b;
+        int dim = Math.min(a.dim, b.dim);
+        double[] x = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            x[i] = a.x[i] + b.x[i];
+        }
+        return new Point(x.length, x);
     }
 
     public Point add(Point b) {
-
+        int mindim = Math.min(dim, b.dim);
+        double[] y = new double[mindim];
+        for (int i = 0; i < mindim; i++) {
+            y[i] = x[i] + b.x[i];
+        }
+        return new Point(y.length, y);
     }
 
     static Point sub(Point a, Point b) {
-
+        int dim = Math.min(a.dim, b.dim);
+        double[] x = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            x[i] = a.x[i] - b.x[i];
+        }
+        return new Point(x.length, x);
     }
 
     Point sub(Point b) {
-
+        int mindim = Math.min(dim, b.dim);
+        double[] y = new double[mindim];
+        for (int i = 0; i < mindim; i++) {
+            y[i] = x[i] - b.x[i];
+        }
+        return new Point(y.length, y);
     }
 
     static Point mult(Point a, double r) {
-
+        double[] x = new double[a.dim];
+        for (int i = 0; i < a.dim; i++) {
+            x[i] = a.x[i] * r;
+        }
+        return new Point(x.length, x);
     }
 
     Point mult(double r) {
-
+        double[] y = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            y[i] = x[i] * r;
+        }
+        return new Point(y.length, y);
     }
 
     static double mult(Point a, Point b) {
-
+        int dim = Math.min(a.dim, b.dim);
+        int res = 0;
+        for (int i = 0; i < dim; i++) {
+            res += a.x[i] * b.x[i];
+        }
+        return res;
     }
 
     double mult(Point b) {
-
+        int mindim = Math.min(dim, b.dim);
+        int res = 0;
+        for (int i = 0; i < mindim; i++) {
+            res += x[i] * b.x[i];
+        }
+        return res;
     }
 
-    static
+    static Point symAxis(Point a, int i) {
+        double[] y = new double[a.dim];
+        for (int k = 0; k < a.x.length; k++) {
+            if(k != i)
+                y[k] = a.x[k] * -1;
+        }
+        return new Point(y.length, y);
+    }
+
+    Point symAxis(int i) {
+        double[] y = new double[dim];
+        for (int k = 0; k < x.length; k++) {
+            if(k != i)
+                y[k] = x[k] * -1;
+        }
+        return new Point(y.length, y);
+    }
+
+//    String toString(){
+//
+//    }
 }
 
+/*
 class Point2D extends Point {
-    public Point2D(int dim) {
-        super(dim);
+    public Point2D() {
+
     }
 
-    public Point2D(int dim, double[] x) {
+    public Point2D(double[] x) {
         super(dim, x);
+        new Point(2, x);
     }
 }
 
 class Point3D extends Point {
-
     public Point3D(int dim) {
         super(dim);
+        double[]x = new double[]{0, 0, 0};
+        new Point(dim, x);
     }
 
     public Point3D(int dim, double[] x) {
         super(dim, x);
+        new Point(dim, x);
     }
 }
+
+ */
